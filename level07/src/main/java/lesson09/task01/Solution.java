882f95c0efe4fd3fcc1fbd9607cdac533b271c69
kirillkrohmal <krohmal_kirill@mail.ru>
2022-02-15 21:53:45 +0300

    fiveth commit

--- level07/src/main/java/lesson09/task01/Solution.java
+++ level07/src/main/java/lesson09/task01/Solution.java
@@ -11,7 +11,8 @@ import java.util.List;
 Число делится на 3 (x%3==0), делится на 2 (x%2==0) и все остальные.
 Числа, которые делятся на 3 и на 2 одновременно, например 6, попадают в оба списка.
 2. Метод printList должен выводить на экран все элементы списка  с новой строки.
-3. Используя метод printList выведи эти три списка на экран. Сначала тот, который для x%3, потом тот, который для x%2, потом последний.
+3. Используя метод printList выведи эти три списка на экран. Сначала тот, который для x%3,
+потом тот, который для x%2, потом последний.
 */
 
 public class Solution {
@@ -19,12 +20,37 @@ public class Solution {
         //add your code here
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> listAliquot3 = new ArrayList<Integer>();
+        ArrayList<Integer> listAliquot2 = new ArrayList<Integer>();
+        ArrayList<Integer> listOther = new ArrayList<Integer>();
 
+        for (int i = 0; i < 20; i++) {
+            String s = reader.readLine();
+            list.add(Integer.valueOf(s));
+        }
 
+        for (Integer integer : list) {
+            if ((integer%2==0 && integer%3==0)) {
+                listAliquot2.add(integer);
+                listAliquot3.add(integer);
+            } else if(integer%3==0) {
+                listAliquot3.add(integer);
+            } else if(integer%2==0) {
+                listAliquot2.add(integer);
+            } else {
+                listOther.add(integer);
+            }
+        }
+        printList(listAliquot3);
+        printList(listAliquot2);
+        printList(listOther);
     }
+    
 
     public static void printList(List<Integer> list) {
         //add your code here
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
     }
-
 }
