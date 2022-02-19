d89acc70ec5f51e1247fa1e3a7fcee694c57a1e8
kirillkrohmal <krohmal_kirill@mail.ru>
2022-02-19 19:40:27 +0300

    third commit

--- level10/src/main/java/lesson11/home05/Solution.java
+++ level10/src/main/java/lesson11/home05/Solution.java
@@ -21,6 +21,8 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         //алфавит
-    }
+        String abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
+
+    }
 }
--- level10/src/main/java/lesson11/home07/Solution.java
+++ level10/src/main/java/lesson11/home07/Solution.java
@@ -1,1 +1,23 @@
 package lesson11.home07;
+
+public class Solution {
+    public static int C;
+    public int A = 5;
+    public static int B = 5; // static ???
+    public static int D = 5;
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.A = 5;
+        solution.B = 5 * B;
+        solution.C = 5 * C * D;
+        Solution.D = 5 * D * C;
+
+        Solution.D = 5;
+    }
+
+    public int getA() {
+        return A;
+    }
+
+}
