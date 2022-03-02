04cfcc3a55da434fdb93c3ff9fe8dd74755a1852
kirillkrohmal <krohmal_kirill@mail.ru>
2022-03-02 21:23:22 +0300

    third commit

--- level22/src/main/java/lesson18/big01/Tetris.java
+++ level22/src/main/java/lesson18/big01/Tetris.java
@@ -1,4 +1,41 @@
 package lesson18.big01;
 
 public class Tetris {
+    public Field field;
+    public Figure figure;
+    public static Tetris game;
+
+    public static void main(String[] args) {
+        game = new Tetris();
+        game.run();
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
+
+    public void run() {
+
+    }
+
+    public void step() {
+
+    }
+
+    public static Tetris game() {
+
+        return null;
+    }
 }
