a428b66715941fe7330a20319e12cc2ee0d549c3
kirillkrohmal <krohmal_kirill@mail.ru>
2022-03-08 20:18:59 +0300

    third commit

--- level24/src/main/java/lesson14/big01/com/javarush/test/level24/lesson14/big01/Ball.java
+++ level24/src/main/java/lesson14/big01/com/javarush/test/level24/lesson14/big01/Ball.java
@@ -9,9 +9,14 @@ public class Ball extends BaseObject {
 
     private boolean isFrozen;
 
+    public Ball(double x, double y, double speed, double direction)
+    {
+        super(x, y, 1);
 
-    public Ball(double x, double y, double radius) {
-        super(x, y, radius);
+        this.direction = direction;
+        this.speed = speed;
+
+        this.isFrozen = true;
     }
 
     public double getSpeed() {
@@ -26,8 +31,13 @@ public class Ball extends BaseObject {
         return direction;
     }
 
-    public void setDirection(double direction) {
+    public void setDirection(double direction)
+    {
         this.direction = direction;
+
+        double angel = Math.toRadians(direction);
+        dx = Math.cos(angel) * speed;
+        dy = -Math.sin(angel) * speed;
     }
 
     public double getDx() {
@@ -56,13 +66,49 @@ public class Ball extends BaseObject {
 
     @Override
     public void draw(Canvas canvas) {
-
+        canvas.setPoint(x, y, 'O');
     }
 
     @Override
     public void move() {
+        if(isFrozen) return;
 
+        x += dx;
+        y += dy;
+
+        checkRebound(1, Arcanoid.game.getWidth(),1, Arcanoid.game.getHeight() + 5);
+    }
+
+    public void checkRebound(int minx, int maxx, int miny, int maxy)
+    {
+        if (x < minx)
+        {
+            x = minx + (minx - x);
+            dx = -dx;
+        }
+
+        if (x > maxx)
+        {
+            x = maxx - (x - maxx);
+            dx = -dx;
+        }
+
+        if (y < miny)
+        {
+            y = miny + (miny - y);
+            dy = -dy;
+        }
+
+        if (y > maxy)
+        {
+            y = maxy - (y - maxy);
+            dy = -dy;
+        }
     }
 
 
+    public void start() {
+        this.setDirection(direction);
+        this.isFrozen = false;
+    }
 }
--- level24/src/main/java/lesson14/big01/com/javarush/test/level24/lesson14/big01/Stand.java
+++ level24/src/main/java/lesson14/big01/com/javarush/test/level24/lesson14/big01/Stand.java
@@ -1,9 +1,24 @@
 package lesson14.big01.com.javarush.test.level24.lesson14.big01;
 
 public class Stand  extends BaseObject {
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+
+    private static int[][] matrix = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
+
+    public Stand(double x, double y)
+    {
+        super(x,y,3);
+        speed = 1;
+        direction = 0;
     }
+
+    private double speed = 1;
+    private double direction = 0;
 
     @Override
     public void draw(Canvas canvas) {
@@ -14,4 +29,13 @@ public class Stand  extends BaseObject {
     public void move() {
 
     }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
 }
